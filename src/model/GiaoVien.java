1c8a13974413eed9c1ed4095a1180fc658752182
Thanhit0903 <nguyenthanh.hn090307@gmail.com>
2024-11-15 10:59:00 +0700

    Khởi tạo

--- src/model/GiaoVien.java
+++ src/model/GiaoVien.java
@@ -9,5 +9,53 @@ package model;
  * @author ADMIN
  */
 public class GiaoVien {
+    private String ten;
+    private Integer tuoi;
+    private String lop;
+    private String gioitinh;
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public Integer getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(Integer tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+
+    public String getGioitinh() {
+        return gioitinh;
+    }
+
+    public void setGioitinh(String gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+
+    public GiaoVien(String ten, Integer tuoi, String lop, String gioitinh) {
+        this.ten = ten;
+        this.tuoi = tuoi;
+        this.lop = lop;
+        this.gioitinh = gioitinh;
+    }
+
+    @Override
+    public String toString() {
+        return ten + "," + tuoi + "," + lop + "," + gioitinh;
+    }
     
 }
