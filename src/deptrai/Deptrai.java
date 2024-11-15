3c5f40b2ce882461032666e78e195ebf21e35d9b
Tucpmh <tuccpmth03322@fpt.edu.vn>
2024-11-15 10:56:52 +0700

    fgjfgjfyj

--- src/deptrai/Deptrai.java
+++ src/deptrai/Deptrai.java
@@ -14,8 +14,8 @@ public class Deptrai {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Nah i'd win");
+        
     }
     
 }
