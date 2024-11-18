2a028190134fa9d112aa8ec527da0669b476e129
Pong1298 <phongvth0910@gmail.com>
2024-11-18 10:32:04 +0700

    af

--- src/service/QuanLyGiaoVien.java
+++ src/service/QuanLyGiaoVien.java
@@ -4,10 +4,22 @@
  */
 package service;
 
+import java.util.ArrayList;
+import model.GiaoVien;
+
 /**
  *
  * @author ADMIN
  */
 public class QuanLyGiaoVien {
-    
+
+    ArrayList<GiaoVien> list = new ArrayList<>();
+
+    public QuanLyGiaoVien() {
+        list.add(new GiaoVien("Cao Phan Minh Hanh Tuk", 59, "SD1804", "Khong Biet"));
+        list.add(new GiaoVien("Nguyen Tien Thanh", 35, "SD1803", "Nu"));
+        list.add(new GiaoVien("Nguyen Tuan Tai", 20, "SD1801", "Nam"));
+        list.add(new GiaoVien("Tran Minh Quan", 25, "SD1832", "Ngo"));
+        list.add(new GiaoVien("Bob Impact", 25, "SD1809", "Nam"));
+    }
 }
