abb35ffbad955f4bb133acef26f1e3f50506f0e2
QuanTran4Rel <minhquan.pacific@gmail.com>
2024-11-18 10:35:00 +0700

    Update QuanLyGiaoVien.java

--- src/service/QuanLyGiaoVien.java
+++ src/service/QuanLyGiaoVien.java
@@ -9,5 +9,14 @@ package service;
  * @author ADMIN
  */
 public class QuanLyGiaoVien {
-    
+    ArrayList<GiaoVien> list = new ArrayList<>();
+
+    public QuanLyGiaoVien() {
+        list.add(new GiaoVien("Cao Phan Minh Hanh Tuk", 59, "SD1804", "Khong Biet"));
+        list.add(new GiaoVien("Nguyen Tien Thanh", 35, "SD1803", "Nu"));
+        list.add(new GiaoVien("Nguyen Tuan Tai", 20, "SD1801", "Nam"));
+        list.add(new GiaoVien("Vu Thanh Hai Phong", 25, "SD1812", "Pong"));
+        list.add(new GiaoVien("Bob Impact", 25, "SD1809", "Nam"));
+    }
+
 }
