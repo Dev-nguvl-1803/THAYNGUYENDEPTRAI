b8720110db0ea1313d8a4273c01553ccf3ea5140
Tucpmh <tuccpmth03322@fpt.edu.vn>
2024-12-06 11:14:21 +0700

    Null

--- src/deptrai/Deptrai.java
+++ src/deptrai/Deptrai.java
@@ -4,6 +4,9 @@
  */
 package deptrai;
 
+import java.util.ArrayList;
+import model.GiaoVien;
+
 /**
  *
  * @author ADMIN
@@ -14,7 +17,7 @@ public class Deptrai {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         System.out.println("Nah i'd win");
         System.out.println("Để đây để còn ghi nhớ: khai báo thuộc tính");
         System.out.println("Có bốn thuộc tính đó là: Mã, tên, tuổi và địa chỉ");
@@ -26,49 +29,49 @@ public class Deptrai {
         System.out.println("Nah i'd win");
         System.out.println("update 2");
 
-        System.out.println("拼图一片片失落\n" +
-                            "像枫叶的冷漠\n" +
-                            "墙上的钟默默数着寂寞\n" +
-                            "\n" +
-                            "咖啡飘散过香味\n" +
-                            "剩苦涩陪着我\n" +
-                            "想念的心埋葬我在深夜的脆弱\n" +
-                            "\n" +
-                            "无尽的苍穹满天的星座\n" +
-                            "你的光亮一闪而过\n" +
-                            "只想要记住这永恒的瞬间\n" +
-                            "\n" +
-                            "像流星的坠落灿烂夺去了轮廓\n" +
-                            "这刹那过后世界只是回忆的沙漏\n" +
-                            "像流星的坠落绚丽地点亮了整个星空\n" +
-                            "像你故事在我生命留下不褪色的伤口\n" +
-                            "\n" +
-                            "湖水守候着沉默\n" +
-                            "等待天边的月\n" +
-                            "孤独的水面却漆黑整夜\n" +
-                            "\n" +
-                            "夜雾凝结的泪光\n" +
-                            "被蒸发在角落\n" +
-                            "他无情地遗忘我在追忆的漩涡\n" +
-                            "\n" +
-                            "无尽的苍穹满天的星座\n" +
-                            "你的光亮一闪而过\n" +
-                            "只想要记住这永恒的瞬间\n" +
-                            "\n" +
-                            "像流星的坠落灿烂夺去了轮廓\n" +
-                            "这刹那过后世界只是回忆的沙漏\n" +
-                            "像流星的坠落绚丽地点亮了整个星空\n" +
-                            "像你故事在我生命留下不褪色的伤口\n" +
-                            "\n" +
-                            "在黑夜的尽头\n" +
-                            "是你的捉弄\n" +
-                            "和无声的伤痛\n" +
-                            "燃烧过后 只剩静默\n" +
-                            "\n" +
-                            "像流星的坠落灿烂夺去了轮廓\n" +
-                            "这刹那过后世界只是回忆的沙漏\n" +
-                            "流星坠落绚丽地点亮了整个星空\n" +
-                            "像你故事在我生命留下不褪色的伤口");
+        System.out.println("拼图一片片失落\n"
+                + "像枫叶的冷漠\n"
+                + "墙上的钟默默数着寂寞\n"
+                + "\n"
+                + "咖啡飘散过香味\n"
+                + "剩苦涩陪着我\n"
+                + "想念的心埋葬我在深夜的脆弱\n"
+                + "\n"
+                + "无尽的苍穹满天的星座\n"
+                + "你的光亮一闪而过\n"
+                + "只想要记住这永恒的瞬间\n"
+                + "\n"
+                + "像流星的坠落灿烂夺去了轮廓\n"
+                + "这刹那过后世界只是回忆的沙漏\n"
+                + "像流星的坠落绚丽地点亮了整个星空\n"
+                + "像你故事在我生命留下不褪色的伤口\n"
+                + "\n"
+                + "湖水守候着沉默\n"
+                + "等待天边的月\n"
+                + "孤独的水面却漆黑整夜\n"
+                + "\n"
+                + "夜雾凝结的泪光\n"
+                + "被蒸发在角落\n"
+                + "他无情地遗忘我在追忆的漩涡\n"
+                + "\n"
+                + "无尽的苍穹满天的星座\n"
+                + "你的光亮一闪而过\n"
+                + "只想要记住这永恒的瞬间\n"
+                + "\n"
+                + "像流星的坠落灿烂夺去了轮廓\n"
+                + "这刹那过后世界只是回忆的沙漏\n"
+                + "像流星的坠落绚丽地点亮了整个星空\n"
+                + "像你故事在我生命留下不褪色的伤口\n"
+                + "\n"
+                + "在黑夜的尽头\n"
+                + "是你的捉弄\n"
+                + "和无声的伤痛\n"
+                + "燃烧过后 只剩静默\n"
+                + "\n"
+                + "像流星的坠落灿烂夺去了轮廓\n"
+                + "这刹那过后世界只是回忆的沙漏\n"
+                + "流星坠落绚丽地点亮了整个星空\n"
+                + "像你故事在我生命留下不褪色的伤口");
 
         System.out.println("Random XD");
         System.out.println("Hát cho giờ ra chơi, mà trời mưa rơi những ngày em không thấy nắng");
@@ -77,6 +80,10 @@ public class Deptrai {
         System.out.println("if you sad just go XD, it's better than anytype of tea");
         System.out.println("Em thấy không có bạn, em đứt ngang dây đàn, em sẽ không ghi bàn hôm nay");
     }
-    
-}
+    ArrayList<GiaoVien> list = new ArrayList<>();
 
+    public Deptrai() {
+        list.add(new GiaoVien("Test", 18, "Null", "N/A"));
+    }
+
+}
